543f7048372f95087c5bd5c99bba2ac326b28191
Timon Caspari <timoncaspari@googlemail.com>
2017-04-28 13:31:18 +0200

    Login-Page

---
+++ src/main/java/com/fhdw/config/viewresolution.java
@@ -0,0 +1,49 @@
+package com.fhdw.config;
+
+/**
+ * Created by Timon Caspari on 28.04.2017.
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+import org.thymeleaf.templateresolver.UrlTemplateResolver;
+
+@Configuration
+
+public class viewresolution extends WebMvcConfigurerAdapter{
+    @Bean
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
+        return resolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.addTemplateResolver(urlTemplateResolver());
+        engine.setTemplateResolver(templateResolver());
+        return engine;
+    }
+
+    private TemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setCacheable(false);
+        return resolver;
+    }
+
+    private UrlTemplateResolver urlTemplateResolver(){
+        return new UrlTemplateResolver();
+    }
+}
---
+++ src/main/java/com/fhdw/controller/ControllerLogin.java
@@ -0,0 +1,19 @@
+package com.fhdw.controller;
+
+/**
+ * Created by Timon Caspari on 28.04.2017.
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControllerLogin {
+    @RequestMapping("/")
+
+    public ModelAndView login(){
+        ModelAndView mav = new ModelAndView("login");
+        return mav;
+    }
+}
