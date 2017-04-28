ba7c38a8290469d12e194ffcb32213df042be243
Timon Caspari <timoncaspari@googlemail.com>
2017-04-28 11:21:37 +0200

    Gradle/Spring/Basic-Project

---
+++ src/main/java/com/fhdw/StudentxApplication.java
@@ -0,0 +1,12 @@
+package com.fhdw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentxApplication.class, args);
+	}
+}
