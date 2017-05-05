d3466361302419a4f4af3edfdda3f373775239af
Timon Caspari <timoncaspari@googlemail.com>
2017-05-05 17:14:44 +0200

    interface Mitarbeiter added

---
+++ src/main/java/com/fhdw/interfaces/MitarbeiterRepo.java
@@ -0,0 +1,11 @@
+package com.fhdw.interfaces;
+
+/**
+ * Created by timon on 05.05.2017.
+ */
+
+import com.fhdw.objects.Mitarbeiter;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MitarbeiterRepo extends CrudRepository<Mitarbeiter, Long> {
+}
