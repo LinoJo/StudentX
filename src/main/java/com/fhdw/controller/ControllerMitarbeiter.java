342f9828d918dfc9e09db406f1062f2002c8a19b
Timon Caspari <timoncaspari@googlemail.com>
2017-05-16 01:21:48 +0200

    mitarbeiter add | password hashed | problem with user role

--- src/main/java/com/fhdw/controller/ControllerMitarbeiter.java
+++ src/main/java/com/fhdw/controller/ControllerMitarbeiter.java
@@ -4,13 +4,20 @@ package com.fhdw.controller;
  * Created by linus on 10.05.2017.
  */
 
+import com.fhdw.enums.Role;
+import com.fhdw.interfaces.MitarbeiterRepo;
 import com.fhdw.objects.Mitarbeiter;
 import com.fhdw.services.security.IMitarbeiterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +25,12 @@ import java.util.List;
 public class ControllerMitarbeiter {
     @Autowired
     private IMitarbeiterService mitarbeiterService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private MitarbeiterRepo mitarbeiterRepo;
 
     @RequestMapping("/mitarbeiter")
     public ModelAndView mitarbeiter(){
@@ -42,5 +55,14 @@ public class ControllerMitarbeiter {
         mav.addObject("newMitarbeiter", new Mitarbeiter());
         mav.addObject("allMitarbeiter", mitarbeiterList);
         return mav;
+    }
+
+    @RequestMapping(value = "/mitarbeiter/new", method = RequestMethod.POST)
+    public String saveStudent(@Valid @ModelAttribute("newMitarbeiter") Mitarbeiter mitarbeiter, BindingResult result) {
+        if(!result.hasErrors()){
+            mitarbeiter.setPasswort(passwordEncoder.encode(mitarbeiter.getPasswort()));
+            mitarbeiterRepo.save(mitarbeiter);
+        }
+        return "redirect:/mitarbeiter";
     }
 }
