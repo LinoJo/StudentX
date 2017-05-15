cbd506b14b0de74ec46277f0eab052a96ea2fb83
Timon Caspari <timoncaspari@googlemail.com>
2017-05-16 00:04:55 +0200

    vorbereitung Mitarbeiter-Seite

--- src/main/java/com/fhdw/controller/ControllerMitarbeiter.java
+++ src/main/java/com/fhdw/controller/ControllerMitarbeiter.java
@@ -4,16 +4,28 @@ package com.fhdw.controller;
  * Created by linus on 10.05.2017.
  */
 
+import com.fhdw.objects.Mitarbeiter;
+import com.fhdw.services.security.IMitarbeiterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class ControllerMitarbeiter {
-    @RequestMapping("/mitarbeiter")
+    @Autowired
+    private IMitarbeiterService mitarbeiterService;
 
+    @RequestMapping("/mitarbeiter")
     public ModelAndView mitarbeiter(){
         ModelAndView mav = new ModelAndView("listemitarbeiter");
+
+        List<Mitarbeiter> mitarbeiterList = mitarbeiterService.getAll();
+
+        mav.addObject("newMitarbeiter", new Mitarbeiter());
+        mav.addObject("allMitarbeiter", mitarbeiterList);
         return mav;
     }
 }
--- src/main/java/com/fhdw/services/security/IMitarbeiterService.java
+++ src/main/java/com/fhdw/services/security/IMitarbeiterService.java
@@ -2,9 +2,13 @@ package com.fhdw.services.security;
 
 import com.fhdw.objects.Mitarbeiter;
 
+import java.util.List;
+
 /**
  * Created by Timon Caspari on 10.05.2017.
  */
 public interface IMitarbeiterService {
     Mitarbeiter getMitarbeiterByEmail(String Email);
+
+    List<Mitarbeiter> getAll();
 }
