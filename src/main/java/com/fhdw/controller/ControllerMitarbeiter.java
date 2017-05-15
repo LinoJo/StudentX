312e569026c8f181a5c8f41d8d814ccd2fcf3b70
Timon Caspari <timoncaspari@googlemail.com>
2017-05-16 01:04:52 +0200

    default user and admin removed from mitarbeiter overview

--- src/main/java/com/fhdw/controller/ControllerMitarbeiter.java
+++ src/main/java/com/fhdw/controller/ControllerMitarbeiter.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -23,6 +24,20 @@ public class ControllerMitarbeiter {
         ModelAndView mav = new ModelAndView("listemitarbeiter");
 
         List<Mitarbeiter> mitarbeiterList = mitarbeiterService.getAll();
+
+        List<Mitarbeiter> toRemove = new ArrayList<Mitarbeiter>();
+        for(Mitarbeiter a: mitarbeiterList){
+            if(a.getEmail().equals("admin")){
+                toRemove.add(a);
+            }
+        }
+        for(Mitarbeiter a: mitarbeiterList){
+            if(a.getEmail().equals("user")){
+                toRemove.add(a);
+            }
+        }
+
+        mitarbeiterList.removeAll(toRemove);
 
         mav.addObject("newMitarbeiter", new Mitarbeiter());
         mav.addObject("allMitarbeiter", mitarbeiterList);
