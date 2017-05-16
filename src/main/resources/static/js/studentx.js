383be1d43235b5f2e2f3ed0671d994c22ce0ec16
Linus Jolmes <l.jolmes@gmx.de>
2017-05-16 17:22:18 +0200

    Feierabend

--- src/main/resources/static/js/studentx.js
+++ src/main/resources/static/js/studentx.js
@@ -1,20 +1,23 @@
 /**
- * Created by Timon Caspari on 16.05.2017.
+ * Created by Linus Jolmes on 16.05.2017.
  */
 
 $(document).on("click",".edit_studenten",function() {
-    var html = $("this").parent.innerHTML();
-    alert(html);
-/*
-    $(".modal-Geschlecht_edit").val()
-    $(".modal-Vorname_edit").val()
-    $(".modal-Nachname_edit").val()
-    $(".modal-Email_edit").val()
-    $(".modal-Geburtsdatum_edit").val()
-    $(".modal-PLZ_edit").val()
-    $(".modal-Ort_edit").val()
-    $(".modal-Strasse_edit").val()
-    $(".modal-Activated_edit").val()
-    $(".modal-StudienGruppenID_edit").val()
-    $(".modal-Praxisfirma_edit").val()*/
+
+    var anrede      = $(this).find("#edit_Geschlecht").val();
+    var vorname     = $(this).find("#edit_Vorname").val();
+    var nachname    = $(this).find("#edit_Nachname").val();
+    var email       = $(this).find("#edit_Email").val();
+    var geburtsdatum = $(this).find("#edit_Geburtsdatum").val();
+    var plz         = $(this).find("#edit_PLZ").val();
+    var ort         = $(this).find("#edit_Ort").val();
+    var strasse     = $(this).find("#edit_Strasse").val();
+    /*var aktive    = $(this).find("#edit_Activated").val();*/
+    var studiengruppenID = $(this).find("#edit_StudienGruppenID").val();
+    var praxisfirma = $(this).find("#edit_Praxisfirma").val();
+
+    //$('.modal-anrede_edit').selectedIndex = anrede;
+    $('.modal-vorname_edit').val(vorname);
+    $('.modal-nachname_edit').val(nachname);
+
 });
