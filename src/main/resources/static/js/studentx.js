56c6fbb5c86dabd7709247f24830e8dd9657d878
Linus Jolmes <l.jolmes@gmx.de>
2017-05-16 13:36:25 +0200

    modal load data

--- src/main/resources/static/js/studentx.js
+++ src/main/resources/static/js/studentx.js
@@ -2,7 +2,19 @@
  * Created by Timon Caspari on 16.05.2017.
  */
 
-function showEditModal(MatID) {
-    var editUrl = "/studenten/load/" + MatID;
-    loadEntity(editUrl);
-}
+$(document).on("click",".edit_studenten",function() {
+    var html = $("this").parent.innerHTML();
+    alert(html);
+/*
+    $(".modal-Geschlecht_edit").val()
+    $(".modal-Vorname_edit").val()
+    $(".modal-Nachname_edit").val()
+    $(".modal-Email_edit").val()
+    $(".modal-Geburtsdatum_edit").val()
+    $(".modal-PLZ_edit").val()
+    $(".modal-Ort_edit").val()
+    $(".modal-Strasse_edit").val()
+    $(".modal-Activated_edit").val()
+    $(".modal-StudienGruppenID_edit").val()
+    $(".modal-Praxisfirma_edit").val()*/
+});
