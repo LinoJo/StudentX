6340ee2265fa33862e46014b695cf09bb42ab646
Dennis Nüßing <dennis-nuessing@live.de>
2017-05-16 09:03:02 +0200

    Student ID Generator changes

--- src/main/java/com/fhdw/controller/ControllerExceptionHandling.java
+++ src/main/java/com/fhdw/controller/ControllerExceptionHandling.java
@@ -1,8 +1,10 @@
 package com.fhdw.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class ControllerExceptionHandling {
-
 }
